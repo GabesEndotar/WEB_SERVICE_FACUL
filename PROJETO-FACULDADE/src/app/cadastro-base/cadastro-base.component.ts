e794f5b0e21426e7640d6ec7b09d97718e715aea
gabrielmndnca <gabrielmndnca@gmail.com>
2019-11-29 17:25:41 -0300

    [UPDATE] Atualizações de Grids

--- PROJETO-FACULDADE/src/app/cadastro-base/cadastro-base.component.ts
+++ PROJETO-FACULDADE/src/app/cadastro-base/cadastro-base.component.ts
@@ -12,13 +12,43 @@ export class CadastroBaseComponent implements OnInit {
 
   ngOnInit() {
   }
-  columnDefs = [
-    {field: 'make' },
-    {field: 'model' },
-    {field: 'price'}
+  columnFunc = [
+    {headerName: 'Make', field: 'make', sortable: true, filter:true},
+    {headerName: 'Model', field: 'model', sortable: true, filter:true},
+    {headerName: 'Price', field: 'price', sortable: true, filter:true}
+];
+columnDepart = [
+  {headerName: 'Make', field: 'make', sortable: true, filter:true},
+  {headerName: 'Model', field: 'model', sortable: true, filter:true},
+  {headerName: 'Price', field: 'price', sortable: true, filter:true}
+];
+columnEnd = [
+  {headerName: 'Make', field: 'make', sortable: true, filter:true},
+  {headerName: 'Model', field: 'model', sortable: true, filter:true},
+  {headerName: 'Price', field: 'price', sortable: true, filter:true}
+];
+columnPaises = [
+  {headerName: 'Make', field: 'make', sortable: true, filter:true},
+  {headerName: 'Model', field: 'model', sortable: true, filter:true},
+  {headerName: 'Price', field: 'price', sortable: true, filter:true}
 ];
 
 rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
     { make: 'Porsche', model: 'Boxter', price: 72000 }
