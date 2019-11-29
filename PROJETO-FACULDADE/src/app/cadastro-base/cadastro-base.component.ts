6d02fb2bbfe589d780e590df834126a1e36c4ad2
gabrielmndnca <gabrielmndnca@gmail.com>
2019-11-29 17:33:49 -0300

    [UPDATE] GRID COLUMN NAMES

--- PROJETO-FACULDADE/src/app/cadastro-base/cadastro-base.component.ts
+++ PROJETO-FACULDADE/src/app/cadastro-base/cadastro-base.component.ts
@@ -13,45 +13,40 @@ export class CadastroBaseComponent implements OnInit {
   ngOnInit() {
   }
   columnFunc = [
-    {headerName: 'Make', field: 'make', sortable: true, filter:true},
-    {headerName: 'Model', field: 'model', sortable: true, filter:true},
-    {headerName: 'Price', field: 'price', sortable: true, filter:true}
+    {headerName: 'EMPLOYEE_ID', field: 'EMPLOYEE_ID', sortable: true, filter:true},
+    {headerName: 'FIRST_NAME', field: 'FIRST_NAME', sortable: true, filter:true},
+    {headerName: 'LAST_NAME', field: 'LAST_NAME', sortable: true, filter:true},
+    {headerName: 'EMAIL', field: 'EMAIL', sortable: true, filter:true},
+    {headerName: 'PHONE_NUMBER', field: 'PHONE_NUMBER', sortable: true, filter:true},
+    {headerName: 'HIRE_DATE', field: 'HIRE_DATE', sortable: true, filter:true},
+    {headerName: 'JOB_ID', field: 'JOB_ID', sortable: true, filter:true},
+    {headerName: 'SALARY', field: 'SALARY', sortable: true, filter:true},
+    {headerName: 'COMMISSION_PCT', field: 'COMMISSION_PCT', sortable: true, filter:true},
+    {headerName: 'MANAGER_ID', field: 'MANAGER_ID', sortable: true, filter:true},
+    {headerName: 'DEPARTMENT_ID', field: 'DEPARTMENT_ID', sortable: true, filter:true}
 ];
 columnDepart = [
-  {headerName: 'Make', field: 'make', sortable: true, filter:true},
-  {headerName: 'Model', field: 'model', sortable: true, filter:true},
-  {headerName: 'Price', field: 'price', sortable: true, filter:true}
+  {headerName: 'DEPARTMENT_ID', field: 'DEPARTMENT_ID', sortable: true, filter:true},
+  {headerName: 'DEPARTMENT_NAME', field: 'DEPARTMENT_NAME', sortable: true, filter:true},
+  {headerName: 'MANAGER_ID', field: 'MANAGER_ID', sortable: true, filter:true},
+  {headerName: 'LOCATION_ID', field: 'LOCATION_ID', sortable: true, filter:true}
 ];
 columnEnd = [
-  {headerName: 'Make', field: 'make', sortable: true, filter:true},
-  {headerName: 'Model', field: 'model', sortable: true, filter:true},
-  {headerName: 'Price', field: 'price', sortable: true, filter:true}
+  {headerName: 'LOCATION_ID', field: 'LOCATION_ID', sortable: true, filter:true},
+  {headerName: 'STREET_ADDRESS', field: 'STREET_ADDRESS', sortable: true, filter:true},
+  {headerName: 'POSTAL_CODE', field: 'POSTAL_CODE', sortable: true, filter:true},
+  {headerName: 'CITY', field: 'CITY', sortable: true, filter:true},
+  {headerName: 'STATE_PROVINCE', field: 'STATE_PROVINCE', sortable: true, filter:true},
+  {headerName: 'COUNTRY_ID', field: 'COUNTRY_ID', sortable: true, filter:true}
 ];
 columnPaises = [
-  {headerName: 'Make', field: 'make', sortable: true, filter:true},
-  {headerName: 'Model', field: 'model', sortable: true, filter:true},
-  {headerName: 'Price', field: 'price', sortable: true, filter:true}
+  {headerName: 'COUNTRY_ID', field: 'COUNTRY_ID', sortable: true, filter:true},
+  {headerName: 'COUNTRY_NAME', field: 'COUNTRY_NAME', sortable: true, filter:true},
+  {headerName: 'REGION_ID', field: 'REGION_ID', sortable: true, filter:true}
 ];
 
 rowData = [
     { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
 ];
 
 modules = AllCommunityModules;
