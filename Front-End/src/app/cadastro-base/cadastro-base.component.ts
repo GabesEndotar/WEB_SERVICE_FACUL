7e72bb9c7c497fc9322c91d71a146b936505dd9f
gabrielmndnca <gabrielmndnca@gmail.com>
2019-12-05 01:33:47 -0300

    [UPDATE] Dados de Pesquisa Atualizados

--- Front-End/src/app/cadastro-base/cadastro-base.component.ts
+++ Front-End/src/app/cadastro-base/cadastro-base.component.ts
@@ -19,6 +19,7 @@ export class CadastroBaseComponent implements OnInit {
 
   ngOnInit() {
     this.PreencherConteudo();
+
   }
   PreencherConteudo(){
     this.dataService.getCountries().subscribe(date => this.countries = date);
@@ -28,43 +29,44 @@ export class CadastroBaseComponent implements OnInit {
     this.dataService.getTodos().subscribe(date => this.todos = date);
   }
   columnFunc = [
-    {headerName: 'EMPLOYEE_ID', field: 'EMPLOYEE_ID', sortable: true, filter:true},
-    {headerName: 'FIRST_NAME', field: 'FIRST_NAME', sortable: true, filter:true},
-    {headerName: 'LAST_NAME', field: 'LAST_NAME', sortable: true, filter:true},
-    {headerName: 'EMAIL', field: 'EMAIL', sortable: true, filter:true},
-    {headerName: 'PHONE_NUMBER', field: 'PHONE_NUMBER', sortable: true, filter:true},
-    {headerName: 'HIRE_DATE', field: 'HIRE_DATE', sortable: true, filter:true},
-    {headerName: 'JOB_ID', field: 'JOB_ID', sortable: true, filter:true},
-    {headerName: 'SALARY', field: 'SALARY', sortable: true, filter:true},
-    {headerName: 'COMMISSION_PCT', field: 'COMMISSION_PCT', sortable: true, filter:true},
-    {headerName: 'MANAGER_ID', field: 'MANAGER_ID', sortable: true, filter:true},
-    {headerName: 'DEPARTMENT_ID', field: 'DEPARTMENT_ID', sortable: true, filter:true}
+    {headerName: 'EMPLOYEE_ID', field: 'employee_id', sortable: true, filter:true},
+    {headerName: 'FIRST_NAME', field: 'first_name', sortable: true, filter:true},
+    {headerName: 'LAST_NAME', field: 'last_name', sortable: true, filter:true},
+    {headerName: 'EMAIL', field: 'email', sortable: true, filter:true},
+    {headerName: 'PHONE_NUMBER', field: 'phone_number', sortable: true, filter:true},
+    {headerName: 'HIRE_DATE', field: 'hire_date', sortable: true, filter:true},
+    {headerName: 'JOB_ID', field: 'job_id', sortable: true, filter:true},
+    {headerName: 'SALARY', field: 'salary', sortable: true, filter:true},
+    {headerName: 'COMMISSION_PCT', field: 'commission_pct', sortable: true, filter:true},
+    {headerName: 'MANAGER_ID', field: 'manager_id', sortable: true, filter:true},
+    {headerName: 'DEPARTMENT_ID', field: 'department_id', sortable: true, filter:true}
 ];
 columnDepart = [
-  {headerName: 'DEPARTMENT_ID', field: 'DEPARTMENT_ID', sortable: true, filter:true},
-  {headerName: 'DEPARTMENT_NAME', field: 'DEPARTMENT_NAME', sortable: true, filter:true},
-  {headerName: 'MANAGER_ID', field: 'MANAGER_ID', sortable: true, filter:true},
-  {headerName: 'LOCATION_ID', field: 'LOCATION_ID', sortable: true, filter:true}
+  {headerName: 'DEPARTMENT_ID', field: 'department_id', sortable: true, filter:true},
+  {headerName: 'DEPARTMENT_NAME', field: 'department_name', sortable: true, filter:true},
+  {headerName: 'MANAGER_ID', field: 'manager_id', sortable: true, filter:true},
+  {headerName: 'LOCATION_ID', field: 'location_id', sortable: true, filter:true}
 ];
 columnEnd = [
-  {headerName: 'LOCATION_ID', field: 'LOCATION_ID', sortable: true, filter:true},
-  {headerName: 'STREET_ADDRESS', field: 'STREET_ADDRESS', sortable: true, filter:true},
-  {headerName: 'POSTAL_CODE', field: 'POSTAL_CODE', sortable: true, filter:true},
-  {headerName: 'CITY', field: 'CITY', sortable: true, filter:true},
-  {headerName: 'STATE_PROVINCE', field: 'STATE_PROVINCE', sortable: true, filter:true},
-  {headerName: 'COUNTRY_ID', field: 'COUNTRY_ID', sortable: true, filter:true}
+  {headerName: 'LOCATION_ID', field: 'location_id', sortable: true, filter:true},
+  {headerName: 'STREET_ADDRESS', field: 'street_address', sortable: true, filter:true},
+  {headerName: 'POSTAL_CODE', field: 'postal_code', sortable: true, filter:true},
+  {headerName: 'CITY', field: 'city', sortable: true, filter:true},
+  {headerName: 'STATE_PROVINCE', field: 'state_province', sortable: true, filter:true},
+  {headerName: 'COUNTRY_ID', field: 'country_id', sortable: true, filter:true}
 ];
 columnPaises = [
-  {headerName: 'COUNTRY_ID', field: 'COUNTRY_ID', sortable: true, filter:true},
-  {headerName: 'COUNTRY_NAME', field: 'COUNTRY_NAME', sortable: true, filter:true},
-  {headerName: 'REGION_ID', field: 'REGION_ID', sortable: true, filter:true}
+  {headerName: 'COUNTRY_ID', field: 'country_id', sortable: true, filter:true},
+  {headerName: 'COUNTRY_NAME', field: 'country_name', sortable: true, filter:true},
+  {headerName: 'REGION_ID', field: 'region_id', sortable: true, filter:true}
 ];
-
-rowDataF : any;
-rowDataD : any;
-rowDataE : any;
-rowDataP : any;
+columnTodos = [
+  {headerName: 'NOME', field: 'NOME', sortable: true, filter:true},
+  {headerName: 'DEPARTAMENTO', field: 'DEPARTAMENTO', sortable: true, filter:true},
+  {headerName: 'CIDADE', field: 'CIDADE', sortable: true, filter:true},
+  {headerName: 'PAÍS', field: 'PAIS', sortable: true, filter:true}
+];
 
 modules = AllCommunityModules;
 
-} 
+}
